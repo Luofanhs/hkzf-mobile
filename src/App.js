75d0b6599968259d9a328e82c1893a838d53cc45
luofan <luofanhs6666@163.com>
2019-08-01 22:19:29 +0800

    项目环境搭建

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import City from './pages/City'
+import Map from './pages/Map'
 class App extends React.Component {
   render() {
-    return <div className="app">app</div>
+    return (
+      <Router>
+        <div className="app">app</div>
+        <ul>
+          <li>
+            <NavLink to="/home">首页</NavLink>
+          </li>
+          <li>
+            <NavLink to="/city">城市选择</NavLink>
+          </li>
+          <li>
+            <NavLink to="/map">地图找房</NavLink>
+          </li>
+        </ul>
+        <Route path="/home" component={Home} />
+        <Route path="/city" component={City} />
+        <Route path="/map" component={Map} />
+      </Router>
+    )
   }
 }
 
